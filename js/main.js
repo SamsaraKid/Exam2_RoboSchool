90a202883065b9bf7b931f8959e39801fc3503d7
koany <koroliev@yandex.ru>
2023-05-14 20:29:02 +0300

    В первой итерации обработка формы сделана

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
+// $(document).ready($('.textField').val(''))
+let forma = document.getElementById('form')
 let menuIsOpen = false
 
+forma.onsubmit = getData
 
 
+// Изменение стиля плавающего меню в зависимости от прокрутки
 function consoleStyle(){
     if ($(window).scrollTop() == 0 && $('header').css('position') == 'fixed'){
         $('#console').css('background', 'none')
@@ -13,7 +17,9 @@ function consoleStyle(){
         $('#wideMenu a').css('color', 'black')
     }
 }
+consoleStyle()
 
+// Показать-скрыть меню
 function showHideMenu(){
 
     if (!menuIsOpen) {
@@ -33,6 +39,7 @@ function showHideMenu(){
         }, 300)
 }
 
+// Показать скрыть длинное описание
 function showHideLongDesc(){
 
     if ($('#longDesc').prop('hidden')){
@@ -43,10 +50,54 @@ function showHideLongDesc(){
 
 }
 
-consoleStyle()
+
+
+// Забираем данные формы
+function getData(){
+    let string = ''
+    let elems = forma.elements
+    for (e in elems) {
+        if (elems[e].type == 'checkbox' && elems[e].checked) {
+            string += elems[e].name
+            string += ': Согласие получено' + '\n'
+        }
+        if (elems[e].type == 'radio' && elems[e].checked) {
+            string += elems[e].name
+            string += ': ' + elems[e].value + '\n'
+        }
+
+
+        if (e == forma.elements.length-1) {break}
+        if (elems[e].name == '') {continue}
+        if (elems[e].value == undefined) {continue}
+
+        if (elems[e].type != 'checkbox' && elems[e].type != 'radio') {
+            string += elems[e].name
+            string += ': ' + elems[e].value + '\n'
+        }
+    }
+    console.log(string)
+    saveToPC(string)
+    return false
+
+}
+
+function saveToPC(str){
+    let blob = new Blob([str], {type: "text/plain"});
+    let link = document.createElement("a");
+    link.setAttribute("href", URL.createObjectURL(blob));
+    link.setAttribute("download", "Client_Data.txt");
+    link.click();
+}
+
+
 
 $(window).scroll(consoleStyle)
 $(window).resize(consoleStyle)
 
 $('#menuBut').click(showHideMenu)
 $('#moreBut').click(showHideLongDesc)
+
+
+
+
